34375b0fe5633e495ed2742fb9b18006c173b182
Owen Utley <owen.utley17@gmail.com>
2021-11-12 16:13:11 -0700

    added update and update random function

--- ratings.py
+++ ratings.py
@@ -10,7 +10,7 @@ user_input = ""
 
 
 dictionary = {}
-
+sorted_dictionary = {}
 
 for i in scores:
     i = i.rstrip().split(":")
@@ -20,8 +20,9 @@ for i in scores:
 
 
 def see():
-    for i in dictionary:
-        print(i, "is rated at", dictionary[i])
+    for i in sorted(dictionary):
+        sorted_dictionary[i] = dictionary[i]
+        print(i, "is rated at", sorted_dictionary[i])
 
 
 def add():
@@ -40,23 +41,57 @@ def add():
         sorted_dictionary[i] = dictionary[i]
         print(i, "is rated at", sorted_dictionary[i])
 
-def update():
+def update_random():
     num = random.randrange(0, len(dictionary))
     new_value = list(dictionary.items())
     str = new_value[num]
+    new_rating = 0
+    print(F"The random restaurant selected is {str[0]} : current rating is {str[1]}")
+    while new_rating < 1 or new_rating > 5:
+        new_rating = int(input(F"(rate {str[0]} out of 5)"))
+        if new_rating < 1 or new_rating > 5:
+            print("You must use a number from 1 to 5")
 
-    # print(dictionary.values()[num])
+
+    dictionary[str[0]] = (new_rating)
     
+def update():
+    restaurant_name = "1l23k4jh"
+    
+    while not restaurant_name in dictionary:
+        for i in sorted(dictionary):
+            print(i)
+        if not restaurant_name in dictionary and restaurant_name != "1l23k4jh":
+            print("You must update a restaurant from the list")
+        restaurant_name = input("What restaurant would you like to rate?\n"
+            "(Type restaurant name here)")
+        if not restaurant_name in dictionary:
+            print("You must update a restaurant from the list")
+    new_rating = 0
+    print(F"Previous rating for {restaurant_name} is {dictionary.get(restaurant_name)}")
+    while new_rating < 1 or new_rating > 5:
+        new_rating = int(input(F"(Give rating for {restaurant_name} out of 5)"))
+        dictionary[restaurant_name] = (new_rating)
+        if new_rating < 1 or new_rating > 5:
+            print("You must use a number from 1 to 5")
 
-while user_input != "quit":
-    user_input = input("To see restaurants and their ratings, type : see\n"
-        "To add a new restaurant and rating type : add\n"
-        "To update the rating of a random restaurant type : update\n"
-        "To quit type : quit\n")
 
-    if user_input == "see":
-        see()
-    if user_input == "add":
-        add()
-    if user_input == "update":
-        update()
+
+def start():
+    global user_input
+    while user_input != "quit":
+        user_input = input("To see restaurants and their ratings, type : see\n"
+            "To add a new restaurant and rating type : add\n"
+            "To update the rating of a random restaurant type : random\n"
+            "To update the rating of a restaurant fo choice type : update\n"
+            "To quit type : quit\n")
+
+        if user_input == "see":
+            see()
+        if user_input == "add":
+            add()
+        if user_input == "random":
+            update_random()
+        if user_input == "update":
+            update()
+start()
